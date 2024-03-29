ee75767cb0718ffac5327410d3cf058dc7435ec4
beuchehmaxime <maximebeucheh86@gmail.com>
2024-03-29 20:28:20 +0100

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,18 +2,22 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../customhooks/useGames';
 import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
 
 
 
 function GameGrid() {
-    const {games,errors } = useGames();
-   
+    const {games,errors,isLoading} = useGames();
+
+    const skeletons = [1, 2, 3, 4, 5, 6,7,8];
+
     return (
         <>
             {errors && <Text>{ errors }</Text>}
             <SimpleGrid columns={{
                 sm: 1, md: 2, lg: 3, xl: 4,
             }} spacing={10} padding='10px'>
+                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
                 {games.map(game => 
                     <GameCard key={game.id} game={game} />
                 )}
--- src/customhooks/useGames.ts
+++ src/customhooks/useGames.ts
@@ -24,20 +24,26 @@ interface FetchGamesResponse{
 const useGames = () => {
  const [games, setGames] = useState<Game[]>([]);
     const [errors, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
     
     useEffect(() => {
+        setLoading(true);
         const controller = new AbortController();
 
         apiClient.get<FetchGamesResponse>('/games', { signal: controller.signal })
-            .then(res => setGames(res.data.results))
+            .then(res => {
+                setGames(res.data.results);
+                setLoading(false);
+            })
             .catch(error => {
                 if (error instanceof CanceledError) return;
                 setError(error.message);
+                setLoading(false)
             });
         return () => controller.abort();
     }, []);
 
-    return { games, errors };
+    return { games, errors, isLoading };
 }
 
 
