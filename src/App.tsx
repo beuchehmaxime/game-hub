8ee9fe36cfbbacc72805777c4db1b455388d613d
beuchehmaxime <maximebeucheh86@gmail.com>
2024-03-29 11:53:39 +0100

    Added a logo and built a Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react"
+import Navbar from "./components/Navbar"
 
 
 function App() {
@@ -7,7 +8,9 @@ function App() {
       base: `"nav" "main"`,
       lg: `"nav nav" "aside main"`
     }}>
-      <GridItem area='nav' bg='blue'>Nav</GridItem>
+      <GridItem area='nav' bg='blue'>
+        <Navbar/>
+      </GridItem>
       <Show above="lg">
         <GridItem area='aside' bg='black'>Aside</GridItem>
       </Show>
