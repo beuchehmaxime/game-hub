c90cf0cb42376faa7663a650e47e4757492489e9
beuchehmaxime <maximebeucheh86@gmail.com>
2024-03-31 05:57:43 +0100

    Refactored code by adding the game query object and removing the genre and platform use states and turning it into the games query object

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,14 +5,17 @@ import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
 import { Genre } from '../customhooks/useGenres';
+import { GameQuery } from '../App';
 
 interface Props{
-    selectedGenre: Genre | null;
-    selectedPlatform: Platform | null;
+    gameQuery: GameQuery | null;
+    // selectedGenre: Genre | null;
+    // selectedPlatform: Platform | null;
 }
 
-function GameGrid({selectedGenre, selectedPlatform}:Props) {
-    const {data,errors,isLoading} = useGames(selectedGenre, selectedPlatform);
+function GameGrid({gameQuery}:Props) {
+    // const {data,errors,isLoading} = useGames(selectedGenre, selectedPlatform);
+    const {data,errors,isLoading} = useGames(gameQuery!);
 
     const skeletons = [1, 2, 3, 4, 5, 6,7,8];
 
